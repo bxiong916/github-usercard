ec4025bd5b727caa82bf00e332a16c9f919ad1ad
Bill Xiong <xiong.bill@yahoo.com>
2020-07-16 21:00:29 -0700

    added final

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -124,7 +124,7 @@ const cardsAttach = document.querySelector('.cards');
   }
 
   // rendering my card 
-  axios.get('https://api.github.com/users/samcode206')
+  axios.get('https://api.github.com/users/bxiong916')
   .then(response => {
 
     const gitData = response.data;
@@ -154,85 +154,3 @@ instructors.forEach(inst => {
       console.log('Here is the problem', error)
     })
 })
-
-
-// const cardsClass = document.querySelector('.cards');
-//                     /* original:obj */
-// function createUserCard(object){
-
-//   /* DOM Elements */
-//   const card = dcoument.createElement('div');
-//   const userImg = document.createElement('img');
-//   const cardInfo = document.createElement('div');
-//   const name = document.createElement('h3');
-//   const userName = document.createElement('p');
-//   const location = document.createElement('p');
-//   const profile = document.createElement('p');
-//   const profileAddress = document.createElement('a')
-//   const followers = document.createElement('p');
-//   const following = document.createElement('p');
-//   const bio = document.createElement('p');
-
-//   /* Classes  */
-//   card.className = ('card');
-//   cardInfo.className = ('card-info');
-//   name.className = ('name');
-//   userName.className = ('username');
-
-//   /* Contents */
-//   userImg.setAttribute('src', obj.avatar_url);
-//   name.textContentf = obj.name;
-//   userName.textContent = obj.login;
-//   location.textContent = obj.location;
-//   profile.textContent = 'Profile: '
-//   profileAddress.textContent = obj.html_url;
-//   followers.textContent = `Followers: ${obj.followers}`;
-//   following.textContent = `Following: ${obj.following}`;
-//   bio.textContent = `Bio: ${obj.bio}`;
-
-//   /* Structures */
-//   card.appendChild(userImg);
-//   card.appendChild(cardInfo);
-//   cardInfo.appendChild(name);
-//   cardInfo.appendChild(userName);
-//   cardInfo.appendChild(location);
-//   cardInfo.appendChild(profile);
-//   profile.appendChild(profileAddress);
-//   cardInfo.appendChild(followers);
-//   cardInfo.appendChild(following);
-//   cardInfo.appendChild(bio);
-
-//   return card;
-
-// }
-//  /* Render My Card */
-//  Axios.get(https://api.github.com/bxiong/)
-//  .then(response => {
-
-//   const gitData = response.data;
-
-//   cardsAttach.appendChild(createUserCard(gitData));
-
-//  })
-
-//  .catch(error => {
-//    console.log('Here is the problem', error);
-//  })
-
-//  /* Render Instructor Cards   */
-
-//  const instructors = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell',]
-
-//  instructors.forEach(inst => {
-//    Axios.get(`https://api.github.com/users/${inst}`)
-//     .then(response => {
-    
-//     const gitData = response.data;
-
-//     cardsAttach.appendChild(createUserCard(gitData));
-// })
-
-// .catch(error => {
-//   console.log('here is the problem', error) 
-//   })
-// })  
